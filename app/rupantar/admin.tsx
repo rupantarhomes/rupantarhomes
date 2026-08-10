ff7193f5638abce52bafb4114101acc6b55b9888
rupantarhomes <rupantarhomes@gmail.com>
2026-08-11 01:01:59 +0545

    Update admin.tsx

--- app/rupantar/admin.tsx
+++ app/rupantar/admin.tsx
@@ -495,3 +495,4 @@ function SettingField({ label, type = "text", value, onChange }: { label: string
 export function resetWorkForm(setWorkForm: Dispatch<SetStateAction<WorkForm>>) {
   setWorkForm(emptyWork);
 }
+
