9bd1e03592a07fdde746bb84c8fc9270cf01a77c
rupantarhomes <rupantarhomes@gmail.com>
2026-08-17 16:42:27 +0545

    Remove custom smooth scrolling
    Restore the native scrolling behavior used before the custom wheel-scrolling enhancement.

--- app/client-entry.tsx
+++ app/client-entry.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./globals.css";
-import "./smooth-scroll";
 import { BrandIntro } from "./rupantar/brand-intro";
 import { RupantarSite } from "./rupantar/site";
 
